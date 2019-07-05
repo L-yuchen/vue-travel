a5d382b2a26f55cc541e78700eb97f5a7d86176c
Mr-Lee <1450454649@qq.com>
2019-07-05 23:00:51 +0800

    content

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-content></home-content>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeContent from './components/Content'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeContent
   }
 }
 </script>
---
+++ src/pages/home/components/Content.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class='contetns'>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+    <div class='cont'>
+      <div class='cont-img'>
+        <img class='cont-img-content' src="https://s.qunarzz.com/homenode/images/touchheader/piao.png"/>
+      </div>
+      <p class="cont-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeContent'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  .contetns
+    height: 0
+    overflow: hidden
+    padding-bottom: 50%
+    .cont
+      height: 0
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      .cont-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+
+        box-sizing: border-box
+        padding: .1rem
+        .cont-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .cont-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        color: #333
+        text-align: center
+</style>
