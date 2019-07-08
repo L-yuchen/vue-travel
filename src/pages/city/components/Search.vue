a38345b5295249cc00f181c70a4fa593a6f0c61a
Mr-Lee <1450454649@qq.com>
2019-07-08 11:46:58 +0800

    reset add use vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -10,16 +10,12 @@
     </div>
     <div class="search-content" ref="search" v-show="keyword">
       <ul>
-<<<<<<< HEAD
-        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
-=======
         <li
           class="search-item border-bottom"
           v-for="item of list"
           :key="item.id"
           @click="handleCityClick(item.name)"
         >
->>>>>>> city-vuex
           {{item.name}}
         </li>
         <li class="search-item border-bottom" v-show="hasNoData">
@@ -31,10 +27,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-=======
 import { mapMutations } from 'vuex'
->>>>>>> city-vuex
 import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
@@ -77,8 +70,6 @@ export default {
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.search)
-<<<<<<< HEAD
-=======
   },
   methods: {
     handleCityClick (city) {
@@ -87,7 +78,6 @@ export default {
       this.$router.push('/')
     },
     ...mapMutations(['changeCity'])
->>>>>>> city-vuex
   }
 }
 </script>
