2d8d0c0e3121ca49edd32b92a8ffaa734836f979
Mr-Lee <1450454649@qq.com>
2019-07-08 09:58:57 +0800

    城市列表性能优化

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -32,8 +32,15 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  // 当页面数据被更新的时候,页面渲染完毕就会执行updated
+  updated () {
+    // 高度是固定的,如果放在移动事件里,每次移动都会触发计算,用生命周期钩子函数就可以解决,此钩子只要页面重新渲染才会执行
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick (e) {
@@ -44,12 +51,17 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        // 通过函数节流方式 提高性能 当用户正在执行移动事件时,先判断是否正在执行,如果再次执行先清理上一次的操作
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
